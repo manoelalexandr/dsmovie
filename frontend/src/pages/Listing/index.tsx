d2d8036bc519b0317c647d10c852a7f9e98eb3a0
manoelalexandr <manoelalexandr@gmail.com>
2022-05-11 22:54:29 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,43 @@
+import MovieCard from "components/Navbar/MovieCard";
 import Pagination from "components/Navbar/Pagination";
 
 function Linting() {
-    return(
-        <Pagination />
+    return (
+
+        <>
+            <Pagination />
+
+            <div className="container">
+
+                <div className="row">
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                   
+                </div>
+
+            </div>
+
+
+
+        </>
     )
 }
 
