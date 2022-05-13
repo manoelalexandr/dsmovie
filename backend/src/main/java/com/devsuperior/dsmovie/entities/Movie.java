fdd9ecdf0dd7d74ef7c3b57d4e9c6f51059acd4d
manoelalexandr <manoelalexandr@gmail.com>
2022-05-12 21:47:19 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsmovie.entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String image;
+
+    public Movie(){
+    }
+
+    public Movie(Long id, String title, Double score, Integer count, String image) {
+        this.id = id;
+        this.title = title;
+        this.score = score;
+        this.count = count;
+        this.image = image;
+    }
+
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsmovie.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Embeddable
+public class ScorePK implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public ScorePK(){
+
+    }
+
+
+}
